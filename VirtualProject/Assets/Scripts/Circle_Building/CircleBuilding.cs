cbc29b2065a43d6a25c0a129c0e8717d7da9ad9f
jaycoap <70280331+jaycoap@users.noreply.github.com>
2021-08-05 20:04:31 +0900

    2021-08-05 PM 20:04
    광호형 HP바 일단 광호형 전용test2

---
+++ VirtualProject/Assets/Scripts/Circle_Building/CircleBuilding.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBuilding : MonoBehaviour
+{
+    public int Circle_Building_maxHealth = 500;
+    public int Circle_Health;
+    public Animator anim;
+
+    public HealthBar Circle_HealthBar;
+
+
+    void Start()
+    {
+        Circle_Health = 0;
+        Circle_HealthBar.SetMaxHealth(Circle_Building_maxHealth);
+        //anim.SetBool("Building_State", true);
+
+    }
+
+    // Update is called once per frame
+    public void Build_Circle()
+    {
+        Circle_Health = Circle_Health  + 100;
+        Circle_HealthBar.SetHealth(Circle_Health);
+    }
+}
