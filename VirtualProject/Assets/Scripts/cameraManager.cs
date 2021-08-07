ee6794814d0a95e9ebc0211fbba5458a8e83fcd6
Printed Love <dnjswn0328@gmail.com>
2021-08-07 22:40:08 +0900

    플레이어 이동, 프리팹화

--- VirtualProject/Assets/Scripts/cameraManager.cs
+++ VirtualProject/Assets/Scripts/cameraManager.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class cameraManager : MonoBehaviour
 {
-    public float cameraSpeed = 5.0f;
-    public GameObject player;
+    public Camera m_Camera;
+    public Transform m_player;
 
-    // Update is called once per frame
+    void Start()
+    {
+    }
+
     void Update()
     {
-        Vector3 dir = player.transform.position - this.transform.position;
-        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
-        this.transform.Translate(moveVector);
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 position = m_player.position;
+        position.Set(m_player.position.x, m_player.position.y, transform.position.z);
+        m_Camera.transform.SetPositionAndRotation(position, Quaternion.identity);
     }
 }
---
+++ VirtualProject/Assets/Scripts/playerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    Vector3 mousePos, mousePosTrans, moveTargetPos;
+    float sprAngle;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            CalMoveTagetPos();
+
+            sprAngle = Mathf.Atan2(mousePosTrans.y - transform.position.y, mousePosTrans.x - transform.position.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(sprAngle - 90, Vector3.forward);
+        }
+
+        MoveToTarget();     //마우스 클릭 위치로 이동
+    }
+
+    void CalMoveTagetPos()
+    {
+        mousePos = Input.mousePosition;
+        mousePosTrans = Camera.main.ScreenToWorldPoint(mousePos);
+        moveTargetPos = new Vector3(mousePosTrans.x, mousePosTrans.y, 0);
+    }
+
+    void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveTargetPos, Time.deltaTime * speed);
+    }
+}
