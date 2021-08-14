f4563bb6f1f8a633411b3b53e411dba22335b03d
Printed Love <dnjswn0328@gmail.com>
2021-08-14 15:34:46 +0900

    픽셀 퍼 유닛 통일, 카메라 추적 스크립트 수정, 콜라이더 추가

--- VirtualProject/Assets/Scripts/cameraManager.cs
+++ VirtualProject/Assets/Scripts/cameraManager.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class cameraManager : MonoBehaviour
 {
-    public Camera m_Camera;
-    public Transform m_player;
+    public float smoothTimeX, smoothTimeY;
+    public Vector2 velocity;
+    public GameObject player;
+    public Vector2 minPos, maxPos;
+    public bool bound;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void Update()
+    private void Update()
     {
-        FollowPlayer();
-    }
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x
+            , ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y
+            , ref velocity.y, smoothTimeY);
 
-    private void FollowPlayer()
-    {
-        Vector3 position = m_player.position;
-        position.Set(m_player.position.x, m_player.position.y, transform.position.z);
-        m_Camera.transform.SetPositionAndRotation(position, Quaternion.identity);
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+        if (bound)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x)
+                , Mathf.Clamp(transform.position.y, minPos.y, maxPos.y)
+                , Mathf.Clamp(transform.position.z, transform.position.z, transform.position.z));
+        }
     }
 }
