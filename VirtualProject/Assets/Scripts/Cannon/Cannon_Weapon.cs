3be56b19f9264697f19a88af410d504984703154
ckh2445 <79975403+ckh2445@users.noreply.github.com>
2021-08-27 00:59:59 +0900

    2021-08-27 (2)
    2021-08-27 (2)

--- VirtualProject/Assets/Scripts/Cannon/Cannon_Weapon.cs
+++ VirtualProject/Assets/Scripts/Cannon/Cannon_Weapon.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -14,10 +13,13 @@ public class Cannon_Weapon : MonoBehaviour
     [SerializeField]
     private float attackRate = 0.5f;
     [SerializeField]
-    private float attackRange = 20.0f;
+    private float attackRange = 2.0f;
     private WeaponState weaponState = WeaponState.SearchTarget;
+    private Transform attackTarget = null;
     private SpiderSpawner spiderSpawner;    //게임에 존재하는 Spider 적 정보 획득
 
+    public Transform Enemy_Dish_AttackPoint;
+    public LayerMask PlayerLayers;
     //private Transform Cannontransform;
     //private Vector3 Cannon;
     //public GameObject Spider;
@@ -27,17 +29,41 @@ public class Cannon_Weapon : MonoBehaviour
     public void Setup(SpiderSpawner spiderSpawner)
     {
         this.spiderSpawner = spiderSpawner;
-
-        ChangeState(WeaponState.SearchTarget);
+       
+        //ChangeState(WeaponState.SearchTarget);
+        StartCoroutine("Enemy_Dish_Attack_Ani");
     }
 
+    private void Update()
+    {
+        //Enemy_Dish_Attack_Ani();
+    }
+
+    public IEnumerator Enemy_Dish_Attack_Ani() //공격 함수 
+    {
+        while (true)
+        {
+            Collider2D[] HitPlayers = Physics2D.OverlapCircleAll(Enemy_Dish_AttackPoint.position, attackRange, PlayerLayers); // Enemy Attack_Point쪽에 닿는 모든것들을 수집 
+
+            foreach (Collider2D player in HitPlayers) // 모든 배열을 foreach로 돌림 
+            {
+                if (player.tag == "Spider") // 배열중 이름이 Player가 있으면 아래 실행 
+                {
+                    SpawnProjectile(player.transform);
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(attackRate);
+
+        }
+    }
 
     public void ChangeState(WeaponState newState)
     {
         StopCoroutine(weaponState.ToString()); //이전에 재생중이던 상태 종료
         weaponState = newState; //상태 변경
         StartCoroutine(weaponState.ToString());
-        Debug.Log(spiderSpawner.SpiderList.Count);
+        //Debug.Log(spiderSpawner.SpiderList.Count);
         //StartCoroutine("SearchTarget");
         //Debug.Log(weaponState.ToString());
     }
@@ -46,15 +72,11 @@ public class Cannon_Weapon : MonoBehaviour
     {
         while (true)
         {
-            //float closestDistSqr = Mathf.Infinity; //제일 가까운 적을 찾기 위해 최초 거리를 최대한 크게 설정
+            float closestDistSqr = Mathf.Infinity; //제일 가까운 적을 찾기 위해 최초 거리를 최대한 크게 설정
 
             Debug.Log(spiderSpawner.SpiderList.Count);
-            yield return null;
-        }
-    }
-            //Debug.Log(spiderSpawner.Spider_count);
 
-            /*for (int i = 0; i < spiderSpawner.SpiderList.Count; ++i)
+            for (int i = 0; i < spiderSpawner.SpiderList.Count; ++i)
             {
                 float distance = Vector3.Distance(spiderSpawner.SpiderList[i].transform.position, transform.position);
 
@@ -95,12 +117,13 @@ public class Cannon_Weapon : MonoBehaviour
 
             yield return new WaitForSeconds(attackRate);
 
-            SpawnProjectile();
+            //SpawnProjectile();
         }
     }
 
-    private void SpawnProjectile()
+    private void SpawnProjectile(Transform attackTarget)
     {
-        Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
-    }*/
+        GameObject clone =  Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
+        clone.GetComponent<ProjectTile>().Setup(attackTarget);
+    }
 }
---
+++ VirtualProject/Assets/Scripts/Cannon/ProjectTile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectTile : MonoBehaviour
+{
+    private Movement2D movement2D;
+    private Transform target;
+
+    public void Setup(Transform target)
+    {
+        movement2D = GetComponent<Movement2D>();
+        this.target = target;
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Spider")) return;
+        if (collision.transform != target) return;
+
+        collision.GetComponent<Spider>().Ondie();
+        Destroy(gameObject);
+
+    }
+
+}
